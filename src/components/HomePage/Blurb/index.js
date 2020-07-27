82c91eaba5f51b01ea8b3fc72ca30a6f75a263d7
bnteau <bnteau@gmail.com>
2020-07-27 17:05:45 +0200

    blurb animation to be corrected

--- src/components/HomePage/Blurb/index.js
+++ src/components/HomePage/Blurb/index.js
@@ -1,43 +1,59 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable no-console */
 /* eslint-disable max-len */
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 import './styles.scss';
 
-const Blurb = () => (
-  <div className="blurb">
-    <div className="blurb__cheeses">
-      <article className="blurb__article">
-        <h3 className="blurb__article--title">Fromages de France</h3>
-        <button
-          type="button"
-          className="blurb__button"
-        >
-          Lire plus
-        </button>
-        <p className="blurb__article--text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. In debitis iure tempore soluta! Iure suscipit ducimus debitis, deserunt similique est iste alias neque, laboriosam ipsam magni qui! Distinctio, corporis quibusdam!
-        </p>
-      </article>
-    </div>
+const Blurb = () => {
+  let open = false;
 
-    <div className="blurb__wines">
-      <article className="blurb__article">
-        <h3 className="blurb__article--title">Vins de France</h3>
-        <button
-          type="button"
-          className="blurb__button"
-        >
-          Lire plus
-        </button>
-        {/* <p className="blurb__article--text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. In debitis iure tempore soluta! Iure suscipit ducimus debitis, deserunt similique est iste alias neque, laboriosam ipsam magni qui! Distinctio, corporis quibusdam!
-        </p> */}
-      </article>
-    </div>
-  </div>
-);
+  const handleOnClick = () => {
+    open = !open;
+    // console.log(open);
+  };
 
-Blurb.propTypes = {};
+  console.log(handleOnClick);
+  const className = open ? 'blurb__article--text-active' : 'blurb__article--text';
+
+  return (
+    <div className="blurb">
+      <div className="blurb__cheeses">
+        <article className="blurb__article">
+          <h3 className="blurb__article--title">Fromages de France</h3>
+          <button
+            type="button"
+            className="blurb__button cheeses"
+            onClick={handleOnClick}
+          >
+            Lire plus
+          </button>
+          <p className={className}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In debitis iure tempore soluta! Iure suscipit ducimus debitis, deserunt similique est iste alias neque, laboriosam ipsam magni qui! Distinctio, corporis quibusdam!
+          </p>
+        </article>
+      </div>
+
+      <div className="blurb__wines">
+        <article className="blurb__article">
+          <h3 className="blurb__article--title">Vins de France</h3>
+          <button
+            type="button"
+            className="blurb__button wines"
+            onClick={handleOnClick}
+          >
+            Lire plus
+          </button>
+          <p className={className}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In debitis iure tempore soluta! Iure suscipit ducimus debitis, deserunt similique est iste alias neque, laboriosam ipsam magni qui! Distinctio, corporis quibusdam!
+          </p>
+        </article>
+      </div>
+    </div>
+  );
+};
+
+// Blurb.propTypes = {};
 
 export default Blurb;
