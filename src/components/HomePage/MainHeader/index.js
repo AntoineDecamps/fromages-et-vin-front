15198b66aa7cf1578064cbb92eadcb4447f8cf85
AntoineDecamps <antoine.decamps@ram-16.com>
2020-07-27 16:04:32 +0200

    MainHeader composition and integration v1

--- src/components/HomePage/MainHeader/index.js
+++ src/components/HomePage/MainHeader/index.js
@@ -1,16 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-import Nav from 'src/components/Nav';
 import Connexion from 'src/components/Connexion';
 
 import './styles.scss';
+import mainImage from 'src/assets/images/glass-plate-small.jpg';
+import logo from 'src/assets/images/logo.png';
 
 const MainHeader = () => (
   <div className="mainHeader">
-    <img src="" alt="" className="mainHeader__image" />
+    <img src={mainImage} alt="" className="mainHeader__image" />
+    <div className="mainHeader__logo">
+      <img src={logo} alt="" className="mainHeader__logoImage" />
+    </div>
     <Connexion />
-    <Nav />
+    <nav className="mainHeader__nav">
+      <Link
+        to="/"
+        className="mainHeader__link"
+        exact
+      >
+        Accueil
+      </Link>
+      <Link
+        to="/vins"
+        className="mainHeader__link"
+        exact
+      >
+        Vins
+      </Link>
+      <Link
+        to="/fromages"
+        className="mainHeader__link"
+        exact
+      >
+        Fromages
+      </Link>
+      <Link
+        to="/contacts"
+        className="mainHeader__link"
+        exact
+      >
+        Contacts
+      </Link>
+    </nav>
   </div>
 );
 
