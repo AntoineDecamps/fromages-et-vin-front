32456b767d3a8540b7215e30a156bf4eb6af9956
bnteau <bnteau@gmail.com>
2020-07-27 11:48:56 +0200

    First model for Blurb Component without css

--- src/components/HomePage/Blurb/index.js
+++ src/components/HomePage/Blurb/index.js
@@ -4,7 +4,18 @@ import PropTypes from 'prop-types';
 import './styles.scss';
 
 const Blurb = () => (
-  <div className="blurb">Blurb</div>
+  <div className="blurb">
+    <div className="blurb__cheeses">
+      <h3 className="blurb__cheeses--title">Fromages de France</h3>
+      <button type="button" className="blurb__button blurb__cheeses--button">Lire plus</button>
+      <img src="" alt="" className="blurb__cheeses--img" />
+    </div>
+    <div className="blurb__wines">
+      <h3 className="blurb__wines--title">Vins de France</h3>
+      <button type="button" className="blurb__button blurb__wines--button">Lire plus</button>
+      <img src="" alt="" className="blurb__wines--img" />
+    </div>
+  </div>
 );
 
 Blurb.propTypes = {};
