30d0b4243b88e647df4631355e7132fae0cd81cc
bnteau <bnteau@gmail.com>
2020-07-28 11:02:59 +0200

    text appears by the click, height doesn't change / responsive layout from 768px

--- src/components/HomePage/Blurb/index.js
+++ src/components/HomePage/Blurb/index.js
@@ -8,46 +8,40 @@ import React from 'react';
 import './styles.scss';
 
 const Blurb = () => {
-  let open = false;
+  let openCheeses = false;
+  let openWines = false;
 
   const handleOnClick = (event) => {
-    open = !open;
     const buttonClicked = event.target;
-    console.log(buttonClicked);
-    buttonClicked.textContent = open === false ? 'Lire plus' : 'Réduire';
+    const textToDisplay = buttonClicked.previousSibling;
+    const titleToUnderline = textToDisplay.previousSibling;
+    // const targetedBlock = buttonClicked.closest('div');
 
-    const textToDisplay = buttonClicked.nextSibling;
-    console.log(textToDisplay);
-    // textToDisplay.className = open === false ? 'blurb__article--text' : 'blurb__article--text-active';
-
-    if (open === false) {
-      textToDisplay.textContent = '';
-    }
-    else {
-      textToDisplay.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In debitis iure tempore soluta! Iure suscipit ducimus debitis, deserunt similique est iste alias neque, laboriosam ipsam magni qui! Distinctio, corporis quibusdam!';
+    if (buttonClicked.classList.contains('cheeses')) {
+      openCheeses = !openCheeses;
+      buttonClicked.textContent = (openCheeses === false) ? 'Lire plus' : 'Réduire';
+      textToDisplay.className = (openCheeses === false) ? 'blurb__article--text' : 'blurb__article--text opened';
+      titleToUnderline.className = (openCheeses === false) ? 'blurb__article--title' : 'blurb__article--title opened';
+      // targetedBlock.className = (openCheeses === false) ? 'blurb__cheeses' : 'blurb__cheeses opened';
     }
 
-    // let textToDisplay = '';
-    // if (open === false) {
-    //   buttonClicked.removeChild(textToDisplay.textContent);
-    // }
-    // else {
-    //   textToDisplay = buttonClicked.insertAdjacentHTML('afterend', '<p class="blurb__article--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. In debitis iure tempore soluta! Iure suscipit ducimus debitis, deserunt similique est iste alias neque, laboriosam ipsam magni qui! Distinctio, corporis quibusdam!</p>');
-    // }
+    if (buttonClicked.classList.contains('wines')) {
+      openWines = !openWines;
+      buttonClicked.textContent = (openWines === false) ? 'Lire plus' : 'Réduire';
+      textToDisplay.className = (openWines === false) ? 'blurb__article--text' : 'blurb__article--text opened';
+      titleToUnderline.className = (openWines === false) ? 'blurb__article--title' : 'blurb__article--title opened';
+      // targetedBlock.className = (openWines === false) ? 'blurb__wines' : 'blurb__wines opened';
+    }
   };
-
-  // /!\ if one button is active after a click, then open will change in true value
-  // in this case, if we click on the second button, it will not work until we click again
-  // Solution (?) : split both the handleOnClick events just to have one event by button. /!\
-
-  // Two options : 1) we change the className of p to display the text by the click
-  // 2) we change the textContent of p
 
   return (
     <div className="blurb">
       <div className="blurb__cheeses">
         <article className="blurb__article">
           <h3 className="blurb__article--title">Fromages de France</h3>
+          <p className="blurb__article--text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In debitis iure tempore soluta! Iure suscipit ducimus debitis, deserunt similique est iste alias neque, laboriosam ipsam magni qui! Distinctio, corporis quibusdam!
+          </p>
           <button
             type="button"
             className="blurb__button cheeses"
@@ -55,13 +49,15 @@ const Blurb = () => {
           >
             Lire plus
           </button>
-          <p className="blurb__article--text"></p>
         </article>
       </div>
 
       <div className="blurb__wines">
         <article className="blurb__article">
           <h3 className="blurb__article--title">Vins de France</h3>
+          <p className="blurb__article--text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. In debitis iure tempore soluta! Iure suscipit ducimus debitis, deserunt similique est iste alias neque, laboriosam ipsam magni qui! Distinctio, corporis quibusdam!
+          </p>
           <button
             type="button"
             className="blurb__button wines"
@@ -69,7 +65,6 @@ const Blurb = () => {
           >
             Lire plus
           </button>
-          <p className="blurb__article--text"></p>
         </article>
       </div>
     </div>
