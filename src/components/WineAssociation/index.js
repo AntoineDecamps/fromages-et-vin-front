27324dc16904e69ed036df34b5878971dfc4d250
AntoineDecamps <antoine.decamps@ram-16.com>
2020-08-09 19:09:47 +0200

    add of slide button and fixing css

--- src/components/WineAssociation/index.js
+++ src/components/WineAssociation/index.js
@@ -3,21 +3,30 @@ import PropTypes from 'prop-types';
 
 import Toggler from 'src/containers/Toggler';
 import Description from 'src/components/WineAssociation/Description';
-
+import { Icon } from 'semantic-ui-react';
 import fromage from './fromagetest.jpeg';
 import './styles.scss';
 
 const WineAssociation = ({ open, associatedProduct }) => (
-  <div className="association">
-    <h2 className="association__title">Mariage proposé</h2>
-    <div className="association__card">
-      <h3 className="association__card__title">{associatedProduct[0].name}</h3>
-      <img src={fromage} alt="" className="association__card__image" />
-      <Toggler />
-      {open && (
-        <Description />
-      )}
+  <div className="flexAssociation">
+    <div className="flexAssociation__icon1">
+      <Icon color="yellow" name="angle left" size="huge" />
     </div>
+    <div className="association">
+      <h2 className="association__title">Mariage proposé</h2>
+      <div className="association__card">
+        <h3 className="association__card__title">{associatedProduct[0].name}</h3>
+        <img src={fromage} alt="" className="association__card__image" />
+        <Toggler />
+        {open && (
+          <Description />
+        )}
+      </div>
+    </div>
+    <div className="flexAssociation__icon2">
+      <Icon color="yellow" name="angle right" size="huge" />
+    </div>
+
   </div>
 );
 
