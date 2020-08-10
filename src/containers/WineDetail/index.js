3ae04be69b38d4ddc0bb4cd4333cf87267ce8a07
bnteau <bnteau@gmail.com>
2020-08-10 11:52:04 +0200

    commented code suppressed

--- src/containers/WineDetail/index.js
+++ src/containers/WineDetail/index.js
@@ -4,10 +4,6 @@ import CheeseDetail from 'src/components/CheeseDetail';
 import { getProductBySlug } from 'src/selectors';
 
 const mapStateToProps = (state, ownProps) => {
-  // const wines = state.wines.winesList;
-  // const cheeses = state.cheeses.cheesesList;
-  // const products = [];
-  // products.push(wines, cheeses);
   const productDetail = getProductBySlug(state.wines.winesList, ownProps.slug);
   return {
     name: productDetail.name,
