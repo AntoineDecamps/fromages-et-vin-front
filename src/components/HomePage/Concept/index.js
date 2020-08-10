97ceb8c10bd389d651de436c9f0d34c40f72cf30
bnteau <bnteau@gmail.com>
2020-08-10 16:47:19 +0200

    small modifications on homepage texts

--- src/components/HomePage/Concept/index.js
+++ src/components/HomePage/Concept/index.js
@@ -6,7 +6,7 @@ import './styles.scss';
 const Concept = () => (
   <div className="concept">
     <h1 className="concept__title">Le mariage parfait</h1>
-    <p className="concept__content animate__animated animate__fadeInRight animate__delay-1s">Il n’y a de secrets moins bien gardés que le mariage parfait vins et fromages. On le sait tous : le bon vin peut sublimer un grand fromage et faire de la dégustation une expérience sensorielle hors du commun. Mais comment choisir le bon vin pour le bon fromage ? Grâce à notre site vous trouverez pour chaque produit un accord lui correspondant qui, ravira les palais des plus fins aux plus novices à la recherche de saveurs exceptionnelles...</p>
+    <p className="concept__content animate__animated animate__fadeInRight animate__delay-1s">Il n’y a de secrets moins bien gardés que le mariage parfait vins et fromages. On le sait tous : le bon vin peut sublimer un grand fromage et faire de la dégustation une expérience sensorielle hors du commun. Mais comment choisir le bon vin pour le bon fromage ? Grâce à notre site vous trouverez pour chaque produit un accord lui correspondant, qui ravira les palais des plus novices aux plus fins à la recherche de saveurs exceptionnelles...</p>
   </div>
 );
 
