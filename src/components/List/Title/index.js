0c2b2d2dc8cdba6232321b2a0ac737d1019d1e09
bnteau <bnteau@gmail.com>
2020-08-11 15:53:27 +0200

    small modifcations on list css

--- src/components/List/Title/index.js
+++ src/components/List/Title/index.js
@@ -1,29 +1,15 @@
+/* eslint-disable no-console */
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import 'src/selectors/list';
-
 import './styles.scss';
 
-const Title = ({ name }) => {
-  console.log(name);
-  // const card = document.querySelector('.card__img');
-  // console.log(card);
-
-  // // if (name === 'Fromages') {
-  // //   card.style.padding = '0';
-  // // }
-
-  // cardStyle();
-
-  return (
-    <h2 className="title">Nos {name}</h2>
-  );
-};
+const Title = ({ name }) => (
+  <h2 className="title">Nos {name}</h2>
+);
 
 Title.propTypes = {
   name: PropTypes.string.isRequired,
-  cardStyle: PropTypes.func.isRequired,
 };
 
 export default Title;
--- src/selectors/list.js
+++
@@ -1,6 +0,0 @@
-const cardStyle = () => {
-  const card = document.querySelector('.card__img');
-  console.log(card);
-};
-
-export default cardStyle;
