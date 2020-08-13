002540b63ee1afc32f59a153a1b0edfd1874a374
bnteau <bnteau@gmail.com>
2020-08-13 09:49:49 +0200

    back to initial code on headers

--- src/middleware/cheese.js
+++ src/middleware/cheese.js
@@ -1,14 +1,11 @@
+/* eslint-disable no-console */
 import axios from 'axios';
 import { GET_CHEESES, saveCheeses } from 'src/actions/cheese';
 
 const cheese = (store) => (next) => (action) => {
   switch (action.type) {
     case GET_CHEESES:
-      axios.get('http://54.152.134.184/fromages-et-vin/Cheese-and-Wine/public/api/cheeses/', {
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
-      })
+      axios.get('http://54.152.134.184/fromages-et-vin/Cheese-and-Wine/public/api/cheeses/')
         .then((response) => {
           // console.log(response);
           store.dispatch(saveCheeses(response.data));
