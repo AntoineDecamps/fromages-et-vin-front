6a2b3df748237eec20faf642c714c3bb7685f370
bnteau <bnteau@gmail.com>
2020-08-13 09:21:58 +0200

    test Access control origin on axios request headers

--- src/middleware/cheese.js
+++ src/middleware/cheese.js
@@ -4,7 +4,11 @@ import { GET_CHEESES, saveCheeses } from 'src/actions/cheese';
 const cheese = (store) => (next) => (action) => {
   switch (action.type) {
     case GET_CHEESES:
-      axios.get('http://54.152.134.184/fromages-et-vin/Cheese-and-Wine/public/api/cheeses/')
+      axios.get('http://54.152.134.184/fromages-et-vin/Cheese-and-Wine/public/api/cheeses/', {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+      })
         .then((response) => {
           // console.log(response);
           store.dispatch(saveCheeses(response.data));
