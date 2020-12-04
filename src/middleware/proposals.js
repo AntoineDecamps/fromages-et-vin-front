dac4c8846bb57f23d510712eb3a08831ce204a3f
bnteau <bnteau@gmail.com>
2020-12-04 15:18:42 +0100

    new Url of API

--- src/middleware/proposals.js
+++ src/middleware/proposals.js
@@ -4,7 +4,7 @@ import { GET_PROPOSALS, saveProposals } from 'src/actions/proposals';
 const proposals = (store) => (next) => (action) => {
   switch (action.type) {
     case GET_PROPOSALS:
-      axios.get('http://54.152.134.184/fromages-et-vin/Cheese-and-Wine/public/api/userproposal')
+      axios.get('http://34.203.201.178/fromages-et-vin/Cheese-and-Wine/public/api/userproposal')
         .then((response) => {
           // console.log(response);
           store.dispatch(saveProposals(response.data));
